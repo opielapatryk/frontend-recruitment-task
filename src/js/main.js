b34a32d3edf74398ae22f3b73ac82ae19fabd8c3
opielapatryk <xpatrykopiela@gmail.com>
2022-06-04 01:33:51 +0200

    Enter

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,20 @@
 const button = document.querySelector("button")
 const alertBox = document.querySelector('.alert')
 const svg = document.querySelector("svg")
-
+const notBox = document.querySelectorAll(".not-box")
 button.addEventListener('click', () => {
     alertBox.style.display = "flex";
 })
 
 svg.addEventListener("click", () => {
     alertBox.style.display = "none";
+})
+
+
+document.addEventListener("click", () => {
+    notBox.forEach(e => {
+        if (e != notBox) {
+            alertBox.style.display = "none";
+        }
+    })
 })
